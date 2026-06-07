b08e92a64cef9c5282408020f09ccfa950550ec5
Cosacko <Cosacko>
2026-06-07 20:22:21 -0300

    modificaciones en main para abrir la sesion de spark,para paralelizar la descarga/parseo de subs y 2 acumuladores para registrar los feeds exitosos/fallidos (ademas de otros pequenos agregados para que no falle al compilar)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,12 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+
 object Main {
   def main(args: Array[String]): Unit = {
+    val spark= SparkSession.builder().appName("Lab3").master("local[*]").getOrCreate()
+    val sc = spark.sparkContext
+
     // Parse command-line arguments
     val cmdArgs = CommandLineArgs.parse(args) match {
       case Some(parsed) => parsed
@@ -11,26 +18,60 @@ object Main {
 
     // Filter out malformed subscriptions (None values)
     val subscriptions = subscriptionOpts.flatten
+    val subscriptionsRDD: RDD[Subscription] = sc.parallelize(subscriptions)
+
+    val feedsSuccess = sc.longAccumulator("feeds exitosos")
+    val feedsFailed = sc.longAccumulator("feeds fallidos")
 
     // Download feeds and parse posts, tracking success/failure
-    val downloadResults = subscriptions.map { subscription =>
-      val feedOpt = FileIO.downloadFeed(subscription.url)
-      val posts = feedOpt.fold(List[Post]())(JsonParser.parsePosts(_, subscription.name))
-      (feedOpt.isDefined, posts)
+    val downloadResults: RDD[Post] = subscriptionsRDD.flatMap{subscription =>
+      
+      try{
+        val feedOpt = FileIO.downloadFeed(subscription.url)
+        if(feedOpt.isDefined){
+          feedsSuccess.add(1)
+          val posts = feedOpt.fold(List[Post]())(JsonParser.parsePosts(_, subscription.name))
+          posts.iterator
+        }
+        else{
+          feedsFailed.add(1)
+          println("Warning: Skipping malformed subscription (missing 'name' or 'url' field)")
+          Iterator.empty
+        }
+      }
+      catch{
+        case _: Exception => 
+          println(s"Warning: Failed to download from'${subscription.name}' (${subscription.url})")
+          Iterator.empty
+      }
     }
 
+    if (downloadResults.isEmpty()){
+      println("Error: No valid subscriptions found")
+      sc.stop()
+      sys.exit(1)
+    }
+
+    // val downloadResults = subscriptions.map { subscription =>
+    //   val feedOpt = FileIO.downloadFeed(subscription.url)
+    //   val posts = feedOpt.fold(List[Post]())(JsonParser.parsePosts(_, subscription.name))
+    //   (feedOpt.isDefined, posts)
+    // }
+
     // Count feed successes/failures
-    val feedsSuccess = downloadResults.count(_._1)
-    val feedsFailed = downloadResults.length - feedsSuccess
+    // val feedsSuccess = downloadResults.count(_._1)
+    // val feedsFailed = downloadResults.length - feedsSuccess
 
     // Flatten all posts and count JSON parse failures
-    val allPosts = downloadResults.flatMap(_._2)
-    val postsSuccess = allPosts.length
-    val postsFailed = downloadResults.count(_._2.isEmpty)
+    val downloadResultsList: List[Post] = downloadResults.collect().toList
+
+    // val allPosts = downloadResults.flatMap(_._2)
+    val postsSuccess = downloadResultsList.length
+    val postsFailed = feedsFailed.value
 
     // Filter empty posts
-    val filteredPosts = Analyzer.filterEmptyPosts(allPosts)
-    val postsFiltered = allPosts.length - filteredPosts.length
+    val filteredPosts = Analyzer.filterEmptyPosts(downloadResultsList)
+    val postsFiltered = downloadResultsList.length - filteredPosts.length
 
     // Calculate average characters in filtered posts
     val totalChars = filteredPosts.map(post => post.title.length + post.selftext.length).sum
@@ -38,10 +79,10 @@ object Main {
 
     // Prepare statistics
     val stats = Map(
-      "feedsSuccess" -> feedsSuccess,
-      "feedsFailed" -> feedsFailed,
+      "feedsSuccess" -> feedsSuccess.value.toInt,
+      "feedsFailed" -> feedsFailed.value.toInt,
       "postsSuccess" -> postsSuccess,
-      "postsFailed" -> postsFailed,
+      "postsFailed" -> postsFailed.toInt,
       "postsFiltered" -> postsFiltered,
       "avgChars" -> avgChars
     )
